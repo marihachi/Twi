0796a502317d1587808c82c08cec51967ddefde9
marihachi <marihachi0620@gmail.com>
2018-06-25 00:44:26 +0900

    update example

--- Twi.Example/Form1.Designer.cs
+++ Twi.Example/Form1.Designer.cs
@@ -32,17 +32,17 @@
 			this.button2 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
 			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-			| System.Windows.Forms.AnchorStyles.Right)));
 			this.button1.Font = new System.Drawing.Font("メイリオ", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.button1.Location = new System.Drawing.Point(15, 12);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(216, 26);
+			this.button1.Size = new System.Drawing.Size(110, 26);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "アプリ連携...";
 			this.button1.UseVisualStyleBackColor = true;
@@ -52,7 +52,7 @@
 			// 
 			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.button2.Font = new System.Drawing.Font("メイリオ", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.button2.Location = new System.Drawing.Point(130, 75);
+			this.button2.Location = new System.Drawing.Point(274, 97);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(89, 26);
 			this.button2.TabIndex = 1;
@@ -62,33 +62,64 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-			| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.textBox1.Font = new System.Drawing.Font("メイリオ", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.textBox1.Location = new System.Drawing.Point(3, 3);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(216, 66);
+			this.textBox1.Size = new System.Drawing.Size(360, 88);
 			this.textBox1.TabIndex = 3;
 			// 
 			// panel1
 			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.button3);
 			this.panel1.Controls.Add(this.textBox1);
 			this.panel1.Controls.Add(this.button2);
 			this.panel1.Location = new System.Drawing.Point(12, 44);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(222, 104);
+			this.panel1.Size = new System.Drawing.Size(366, 126);
 			this.panel1.TabIndex = 4;
+			// 
+			// button3
+			// 
+			this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button3.Font = new System.Drawing.Font("メイリオ", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button3.Location = new System.Drawing.Point(194, 97);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(74, 26);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "メディア...";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoEllipsis = true;
+			this.label1.Font = new System.Drawing.Font("メイリオ", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(3, 102);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(185, 17);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "メディアが選択されていません";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(246, 160);
+			this.ClientSize = new System.Drawing.Size(390, 182);
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.button1);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(400, 220);
 			this.Name = "Form1";
 			this.Text = "Twi.Example";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -104,5 +135,7 @@
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
 	}
 }
--- Twi.Example/Form1.cs
+++ Twi.Example/Form1.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Net.Http;
 using System.Windows.Forms;
-using Newtonsoft.Json;
-using Twi.Exceptions;
 
 namespace Twi.Example
 {
 	public partial class Form1 : Form
 	{
-
 		private TwiClient Client { get; set; }
+		private string FilePath { get; set; }
 
 		private const string ConsumerKey = "Please_set_ConsumerKey_here";
 		private const string ConsumerSecret = "Please_set_ConsumerSecret_here";
@@ -32,7 +31,8 @@ namespace Twi.Example
 		{
 			try
 			{
-				Process.Start((await Client.GetAuthorizationUrl()).ToString());
+				var authUrl = await Client.GetAuthorizationUrl();
+				Process.Start(authUrl.ToString());
 
 				var dialog = new Dialog();
 				if (dialog.ShowDialog(this) == DialogResult.OK)
@@ -52,21 +52,49 @@ namespace Twi.Example
 		{
 			try
 			{
+				long? mediaId = null;
+				if (FilePath != null)
+				{
+					var fileName = Path.GetFileName(FilePath);
+
+					byte[] buf;
+					using (var f = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+					{
+						buf = new byte[f.Length];
+						await f.ReadAsync(buf, 0, buf.Length);
+					}
+
+					mediaId = await Client.UploadMedia(buf, fileName);
+				}
+
+				var parameters = new Dictionary<string, string> { { "status", textBox1.Text } };
+				if (mediaId != null)
+					parameters.Add("media_ids", mediaId.Value.ToString());
+
 				var res = await Client.Request(
 					HttpMethod.Post,
 					"https://api.twitter.com/1.1/statuses/update.json",
-					new Dictionary<string, string> { { "status", textBox1.Text } });
+					parameters);
 
-				dynamic json = JsonConvert.DeserializeObject(res);
-				if (json.errors != null)
-				{
-					throw new ApplicationException(json.errors[0].message.Value);
-				}
 				MessageBox.Show("投稿しました", "Twi.Example", MessageBoxButtons.OK, MessageBoxIcon.Information);
 			}
 			catch (TwitterException ex)
 			{
 				MessageBox.Show($"投稿に失敗しました\r\n{ex.Message}\r\n{ex.StackTrace}", "Twi.Example", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			FilePath = null;
+			label1.Text = "メディアが選択されていません";
+
+			var dialog = new OpenFileDialog();
+			dialog.Filter = "画像ファイル|*.png;*.jpg;*.jpeg";
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				FilePath = dialog.FileName;
+				label1.Text = Path.GetFileName(FilePath);
 			}
 		}
 	}
